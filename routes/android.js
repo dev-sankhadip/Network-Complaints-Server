8e753321faa2ab28623ad3663f71d04345553cfe
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-01-26 21:40:43 +0530

    netinfo api created

--- routes/android.js
+++ routes/android.js
@@ -5,33 +5,34 @@ const { connection }=require('../db/database');
 
 androidRouter.post('/netinfo', (request, response)=>
 {
-    let { address, latitude, logitude,sim_info }=request.body;
-    const state=JSON.parse(address).mAdminArea;
-    sim_info=JSON.parse(sim_info);
-    for(let i=0;i<sim_info.length;i++)
-    {
-        const id=shortid.generate();
-        net_info_arr = [];
-        net_info_arr.push(id);
-        net_info_arr.push(state);
-        net_info_arr.push(latitude);
-        net_info_arr.push(logitude);
-        net_info_arr.push(sim_info[i].cellType);
-        net_info_arr.push(sim_info[i].operatorName);
-        net_info_arr.push(sim_info[i].cellSignalStrength.mSignalStrength);
-        net_info_arr.push(sim_info[i].cellSignalStrength.mBitErrorRate);
-        net_info_arr.push(sim_info[i].cellSignalStrength.mTimingAdvance);
-        const sqlQuery="insert into netinfo(id, state, latitude, longitude, sim1opname, sim1nettype, sim1dbm, sim1asu, timingAdvance) values (?,?,?,?,?,?,?,?,?)";
-        connection.query(sqlQuery,net_info_arr,(err, result)=>
-        {
-            if(err)
-            {
-                console.log(err);
-            }
+    console.log(request.body);
+    // let { address, latitude, logitude,sim_info }=request.body;
+    // const state=JSON.parse(address).mAdminArea;
+    // sim_info=JSON.parse(sim_info);
+    // for(let i=0;i<sim_info.length;i++)
+    // {
+    //     const id=shortid.generate();
+    //     net_info_arr = [];
+    //     net_info_arr.push(id);
+    //     net_info_arr.push(state);
+    //     net_info_arr.push(latitude);
+    //     net_info_arr.push(logitude);
+    //     net_info_arr.push(sim_info[i].cellType);
+    //     net_info_arr.push(sim_info[i].operatorName);
+    //     net_info_arr.push(sim_info[i].cellSignalStrength.mSignalStrength);
+    //     net_info_arr.push(sim_info[i].cellSignalStrength.mBitErrorRate);
+    //     net_info_arr.push(sim_info[i].cellSignalStrength.mTimingAdvance);
+    //     const sqlQuery="insert into netinfo(id, state, latitude, longitude, sim1opname, sim1nettype, sim1dbm, sim1asu, timingAdvance) values (?,?,?,?,?,?,?,?,?)";
+    //     connection.query(sqlQuery,net_info_arr,(err, result)=>
+    //     {
+    //         if(err)
+    //         {
+    //             console.log(err);
+    //         }
 
-            console.log(result);
-        })
-    }
+    //         console.log(result);
+    //     })
+    // }
 })
 
 androidRouter.post('/signup', function(request, response)
@@ -87,7 +88,7 @@ androidRouter.post('/login',(request, response)=>
         {
             if(result[0].password===password)
             {
-                response.status(200).send({ response_code:200, result });
+                response.status(200).send({ user_id:result[0].userid, name:result[0].name });
             }
             else
             {
